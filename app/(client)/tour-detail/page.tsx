12fd15e9e1c76b5989f66a946c535a2a1c83a555
Software Engineer <masterweb1125@gmail.com>
2024-02-13 16:41:13 -0800

    cart_booking_progress

--- app/(client)/tour-detail/page.tsx
+++ app/(client)/tour-detail/page.tsx
@@ -3,6 +3,7 @@ import Client_Hero from "@/src/components/client/hero-section/hero.component";
 import { ExploreWorldCover } from "@/src/utils/images/images";
 import Client_RecomendedTours from "@/src/components/client/tour-detail-page/recomended-tours/recomended-tour.component";
 import Client_TourDetailHeader from "@/src/components/client/tour-detail-page/tour-header/tour-header.component";
+import Client_TourDetailDescription from "@/src/components/client/tour-detail-page/tour-details/tour-detail.component";
 const Client_TourDetail = () => {
   return (
     <>
@@ -16,7 +17,7 @@ const Client_TourDetail = () => {
         }
       />
       <Client_TourDetailHeader />
-
+     <Client_TourDetailDescription />
       <Client_RecomendedTours />
     </>
   );
