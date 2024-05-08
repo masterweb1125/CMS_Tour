e8bd1553edbd6d3e424e37968a416a7c0cf04d08
masterweb1125 <masterweb1125@gmail.com>
2024-05-08 09:42:52 +0100

    build and deploy error fixed

--- app/user/blogs/blogdetails/page.tsx
+++ app/user/blogs/blogdetails/page.tsx
@@ -27,7 +27,7 @@ const Client_TourDetail = () => {
           <Grid container mb={3} spacing={3}>
             <Grid item xs={12}>
               <div>
-                <Image src={BlogDetailsImagePost} />
+                <Image src={BlogDetailsImagePost} alt={""} />
               </div>
             </Grid>
             <Grid item xs={12}>
